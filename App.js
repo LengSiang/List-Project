edd1622a6cdf0d94dd524db1413fa4370cc90b43
Dulan <lva04.20@ichat.sp.edu.sg>
2022-06-09 22:59:42 +0800

    Finished list app

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ScrollView, FlatList } from "react-native";
+import names from "./names";
+
+const namesAsObject = names.map((name) => {
+  return {
+    name: name,
+  };
+});
 
 export default function App() {
+  const renderItem = ({ item }) => {
+    return (
+      <View style={styles.listItem}>
+        <Text>{item.name}</Text>
+      </View>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FlatList
+      data={namesAsObject}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.name}
+    />
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  listItem: {
+    padding: 30,
+    margin: 10,
+    backgroundColor: "lightpink",
+    borderBottomColor: "#999",
+    borderBottomWidth: 1,
+    shadowColor: "black",
+    shadowOpacity: 0.2,
+    shadowOffset: { x: 0, height: 3 },
   },
 });
